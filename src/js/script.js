3119b5cc12a5c76c0eec4cf09bd7b0be1eb468a8
ksenyshutova <shutovasn@mail.ru>
2024-09-19 16:47:02 +0300

    new version

--- src/js/script.js
+++ src/js/script.js
@@ -43,12 +43,70 @@ order.addEventListener('click', (e) => {
 let block = document.querySelectorAll('.header__button');
 let main = document.querySelector('.main');
 let announcement = document.querySelector('.section__announcement');
+let headerLink = document.querySelectorAll('.header__menu-link');
+let footerLink = document.querySelectorAll('.footer__menu-link');
 
 block.forEach((button) => {
     button.addEventListener('click', (e) => {
-    e.preventDefault();
-    main.classList.toggle('visually-hidden');
-    announcement.classList.toggle('visually-hidden');
-});
+        e.preventDefault();
+        main.classList.add('visually-hidden');
+        announcement.classList.remove('visually-hidden');
+    });
 });
 
+headerLink.forEach((link) => {
+    link.addEventListener('click', (e) => { // закрытие блока при нажатии блока header
+        e.preventDefault();
+        main.classList.remove('visually-hidden');
+        announcement.classList.add('visually-hidden');
+    });
+});
+
+footerLink.forEach((link) => {
+    link.addEventListener('click', (e) => { // закрытие блока при нажатии блока footer 
+        e.preventDefault();
+        main.classList.remove('visually-hidden');
+        announcement.classList.add('visually-hidden');
+    });
+});
+
+/**
+  Перемещение к нужному блоку
+ */
+
+let project = document.querySelector('.header__menu-link_project');
+let contact = document.querySelector('.header__menu-link_contact');
+let call = document.querySelector('.footer__menu-link_call');
+let truck = document.querySelector('.footer__menu-link_truck');
+let help = document.querySelector('.footer__menu-link_help');
+let cost = document.querySelector('.footer__menu-link_cost');
+
+project.addEventListener('click', (e) => { // перемещение к блоку "о проекте"
+    e.preventDefault();
+    document.querySelector('.section__advantages').scrollIntoView({ behavior: 'smooth' });
+});
+
+contact.addEventListener('click', (e) => { // перемещение к блоку "контакты"
+    e.preventDefault();
+    document.querySelector('.service-card__title_contact').scrollIntoView({ behavior: 'smooth' });
+});
+
+call.addEventListener('click', (e) => { // перемещение к блоку "вызов эвакуатора"
+    e.preventDefault();
+    document.querySelector('.section__hero').scrollIntoView({ behavior: 'smooth' });
+});
+
+truck.addEventListener('click', (e) => { // перемещение к блоку "добавить эвакуатор"
+    e.preventDefault();
+    document.querySelector('.section__information').scrollIntoView({ behavior: 'smooth' });
+});
+
+help.addEventListener('click', (e) => { // перемещение к блоку "помощь"
+    e.preventDefault();
+    document.querySelector('.section__advantages').scrollIntoView({ behavior: 'smooth' });
+});
+
+cost.addEventListener('click', (e) => { // перемещение к блоку "цены"
+    e.preventDefault();
+    document.querySelector('.section__result').scrollIntoView({ behavior: 'smooth' });
+});
